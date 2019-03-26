757d45ec91a73c63a73c0a1e8f4eee6cd31b0361
Bruce Tsvilikhovsky <bruce.tsvilikhovsky@flatironschool.com>
2019-03-26 22:41:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,35 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do | hash_season, contestants |
+      if hash_season == season
+        contestants.each do | contestant |
+          if contestant["status"] == "Winner"
+            winner = contestant['name'].split[0]
+            return winner
+          end
+        end
+      end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do | hash_season, contestants |
+      contestants.each do | contestant |
+        if contestant["occupation"] == occupation
+        return contestant['name']
+      end
+  end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.each do | hash_season, contestants |
+    contestants.each do | contestant |
+      return contestant['hometown']
+    end
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
 end
 
 def get_average_age_for_season(data, season)
